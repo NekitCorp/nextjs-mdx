a88ad76c97523429e6dca24a17572a5c019e3106
NekitCorp <NekitCorp@users.noreply.github.com>
2021-08-22 17:27:05 +0000

    deploy: ca00c4f934573bb2febae90bf489b95d8170592b

---
+++ _next/static/i00vqpKLed2Y5OsU3IV0a/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],"/[[...slug]]":["static/chunks/57-f160764092c9ec402ce6.js","static/chunks/pages/[[...slug]]-48edb8c0c9df41b74b7e.js"],sortedPages:["/_app","/_error","/[[...slug]]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
