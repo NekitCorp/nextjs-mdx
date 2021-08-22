a4217ec82c83b79c017b1eee186e93fa5e190e94
Nikita <warhammer-ss@yandex.ru>
2021-08-22 20:37:28 +0300

    ci(config): :bug: add base path

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
+const isProd = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
 module.exports = {
     reactStrictMode: true,
     images: {
         domains: ["picsum.photos"],
     },
+    basePath: isProd ? "/nextjs-mdx" : "",
 };
